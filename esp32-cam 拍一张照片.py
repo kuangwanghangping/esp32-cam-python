6aade4c5168c12a11ed77f1412d374e861da763e
kuangwanghangping <142001244+kuangwanghangping@users.noreply.github.com>
2024-01-23 20:58:27 +0800

    Add files via upload

---
+++ esp32-cam 拍一张照片.py
@@ -0,0 +1,14 @@
+import camera
+
+
+# 初始化摄像头
+camera.init(0, format=camera.JPEG, fb_location=camera.PSRAM)
+
+# 拍摄一张图片
+buf = camera.capture()  # 大小是640x480
+
+# 保存图片到文件
+with open("第一张图片.png", "wb") as f:
+    f.write(buf)  # buf中的数据就是图片的数据，所以直接写入到文件就行了
+    print("拍照已完成，点击Thonny左侧【MicroPython设备】右侧的三，\n然后看到‘刷新’，点击刷新会看到 图片，\n然后右击图片名称，选择下载到电脑的路径即可...")
+
